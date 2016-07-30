369a1dfc09d6d968e10201318f5c458faeb440d2
mohayonao <mohayonao@gmail.com>
2016-07-30 11:58:58 +0900

    update led

--- src/main/MIDI.js
+++ src/main/MIDI.js
@@ -1,33 +1,44 @@
 "use strict";
 
-const nmap = require("nmap");
 const WebMIDIEmitter = require("web-midi-emitter");
 const DEVICE_MATCHER = "Launch Control";
 const KNOB1_MAP  = [ 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c ];
 const KNOB2_MAP  = [ 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f, 0x30 ];
 const PAD_MAP    = [ 0x09, 0x0a, 0x0b, 0x0c, 0x19, 0x1a, 0x1b, 0x1c ];
+const LED_COLORS = {
+  "R": [ 0x01, 0x02, 0x03 ],
+  "G": [ 0x10, 0x30, 0x30 ],
+  "Y": [ 0x11, 0x22, 0x33 ]
+};
 const KNOB1 = 0, KNOB2 = 1, PAD = 2;
 
 class MIDI {
   constructor(dispatcher, access) {
     this.dispatcher = dispatcher;
     this.device = null;
-    this.ctrl = nmap(8, () => [ 0, 0, 0 ]);
+    this.ctrl = null;
+    this.state = { led: [], ledColor: [
+      LED_COLORS["R"], LED_COLORS["R"], LED_COLORS["R"], LED_COLORS["R"],
+      LED_COLORS["G"], LED_COLORS["G"], LED_COLORS["Y"], LED_COLORS["Y"]
+    ] };
 
     this.device = new WebMIDIEmitter(access, DEVICE_MATCHER);
     this.device.on("statechange", (e) => {
       this.onstatechange(e);
     });
     this.device.on("midimessage", (e) => {
-      this.onmidimessage(e);
+      if (this.ctrl) {
+        this.onmidimessage(e);
+      }
     });
     this.device.send([ 0xb8, 0x00, 0x00 ]);
   }
 
   listen(store) {
     this.store = store;
-    // this.store.on("ctrl", (data) => {
-    // });
+    this.store.on("ctrl:init", (data) => {
+      this.ctrl = data;
+    });
   }
 
   onstatechange(e) {
@@ -57,6 +68,7 @@ class MIDI {
     if (this.ctrl[index][ctrlType] !== value) {
       this.ctrl[index][ctrlType] = value;
       this.dispatcher.emit("ctrl", this.ctrl);
+      this.updateLED();
     }
   }
 
@@ -67,6 +79,32 @@ class MIDI {
   padOff(index) {
     this.updateCtrl(index, PAD, 0);
   }
+
+  updateLED() {
+    if (this.ctrl && this.state.ledColor.length) {
+      PAD_MAP.forEach((pad, index) => {
+        const st = 0x98;
+        const d1 = pad;
+        const d2 = this.computeLEDValue(index);
+
+        if (d2 !== this.state.led[index]) {
+          this.device.send([ st, d1, d2 ]);
+          this.state.led[index] = d2;
+        }
+      });
+    }
+  }
+
+  computeLEDValue(index) {
+    const ctrl = this.ctrl;
+    const ledColor = this.state.ledColor[index];
+
+    return rateAt(ledColor, ctrl[index][KNOB2]) * ctrl[index][PAD];
+  }
+}
+
+function rateAt(list, rate) {
+  return list[Math.max(0, Math.min((list.length * rate)|0, list.length - 1))];
 }
 
 module.exports = MIDI;
